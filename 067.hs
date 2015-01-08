c57fb7dab53141f5a1972257ebdd4d271811e21e
nickspinale <spinalen@carleton.edu>
2015-01-07 20:53:43 -0600

    improved 67

--- 067.hs
+++ 067.hs
@@ -1,4 +1,3 @@
 main = readFile "067.txt" >>= print . foldr1 f . map (map read . words) . lines
 
-f :: [Int] -> [Int] -> [Int]
-f x y = zipWith (+) x $ zipWith max y $ tail y
+f x y = zipWith (+) x . zipWith max y $ tail y
