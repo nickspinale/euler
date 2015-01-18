c52abb8a74b669cf43ff5d717c3484c7682284f1
Nick Spinale <spinalen@carleton.edu>
2015-01-18 05:06:41 -0600

    more solutions

--- 023.hs
+++ 023.hs
@@ -1,22 +1,40 @@
 import Data.List
 import Control.Monad
 
+limit :: Integer
 limit = 28123
 
-main = print $ sum ([1..limit] \\ good)
+main = print . sum $ foldr filter [1..limit] [ \x -> abounds (x - first) | first <- abundants ]
 
-good = [ low + high
-       | low  <- filter abundant [ 1 .. div limit 2 ]
-       , high <- filter abundant [ div limit 2 .. limit ]
-       , low + high <= limit
-       ]
+abundants = filter abounds [1..limit]
 
-abundant n = n < sum (factors n)
+abounds n = n < sum ( init
+                    . foldl (liftM2 (*)) [1]
+                    . map (scanl (*) 1)
+                    . group
+                    $ reduce primes n
+                    )
 
-factors = init . foldl (liftM2 (*)) [1] . map (scanl (*) 1) . group . reduce [2..]
+-- lazyDiff [] _ = []
+-- lazyDiff a [] = a
+-- lazyDiff a@(x:xs) b@(y:ys)
+--     | x > y     =     lazyDiff a  ys
+--     | x < y     = x : lazyDiff xs b
+--     | otherwise =     lazyDiff xs ys
 
-reduce :: [Int] -> Int -> [Int]
+reduce :: [Integer] -> Integer -> [Integer]
 reduce _ 1 = []
-reduce y@(x:xs) n = case mod n x of
+reduce y@(x:xs) n = case n `mod` x of
     0 -> x : reduce y (div n x)
     _ -> reduce xs n
+
+-- Faster than reducing from all numbers
+primes = sieve [2..]
+sieve (p:xs) = p : sieve [ x | x <- xs, x `mod` p > 0 ]
+
+-- abounds n = n < aux (n - 1)
+--   where
+--     aux 0 = 0
+--     aux d = case n `mod` d of
+--               0 -> aux (d - 1) + d
+--               _ -> aux (d - 1)
