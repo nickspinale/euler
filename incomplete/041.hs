acff06cc54e5365816c534c0ca568872b7e8dc24
Nick Spinale <spinalen@carleton.edu>
2015-09-28 22:50:23 -0500

    worked on problem 41

--- incomplete/041.hs
+++ incomplete/041.hs
@@ -1,20 +1,43 @@
-import Data.List
-import Data.Char
+import Control.Monad
 
-digits = ['9','8'..'0']
-limit = 100000
--- limit = 1000000000
+main = print $ sortedIntersect (pandigitals [1..4] ++ pandigitals [1..7]) primes
 
-main = print . find p . reverse $ takeWhile (< limit) primes
+-- p n = length difference + length showed == 10 && isPrefixOf difference digits
+--   where
+--     difference = digits \\ showed
+--     showed = show n 
 
-p n = length difference + length showed == 10 && isPrefixOf difference digits
-  where
-    difference = digits \\ showed
-    showed = show n 
-
+-- pandigital :: Integer -> Boolean
+-- pandigital n = undefined
+    
 -- seperate e x = (e, y) : seperate (succ e) z
 --   where (y, z) = span (< 10 ^ e) x
 
-primes = sieve [2..]
+sortedIntersect :: Ord a => [a] -> [a] -> [a]
+sortedIntersect [] _ = []
+sortedIntersect _ [] = []
+sortedIntersect xxs@(x:xs) yys@(y:ys) = case compare x y of
+    EQ -> x : sortedIntersect xs ys
+    LT -> sortedIntersect xs yys
+    GT -> sortedIntersect xxs ys
 
+inits :: [a] -> [[a]]
+inits [] = [[]]
+inits (x:xs) = [] : map (x:) (inits xs)
+
+pandigitals :: [Integer] -> [Integer]
+pandigitals = fmap (foldl (\n d -> d + 10 * n) 0) . permutations
+
+selections :: [a] -> [(a, [a])]
+selections [] = []
+selections (x:xs) = (x, xs) : fmap (fmap (x:)) (selections xs)
+
+permutations :: [a] -> [[a]]
+permutations [] = [[]]
+permutations xs = do
+    (y, ys) <- selections xs
+    zs <- permutations ys
+    return (y:zs)
+
+primes = sieve [2..]
 sieve (p:xs) = p : sieve [ x | x <- xs, x `mod` p > 0 ]
